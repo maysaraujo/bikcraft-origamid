6cf90f2bb1d99a098eda9cb6b0793610a48a0108
maysaraujo <maysaaraujo25@gmail.com>
2024-07-13 18:53:12 -0300

    feat: making titles and sections dynamics

--- page-portfolio.php
+++ page-portfolio.php
@@ -33,24 +33,9 @@ get_header();
 
 		<section class="portfolio">
 			<div class="container">
-				<ul class="portfolio_lista rslides_portfolio">
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-				</ul>
+				<?php include(TEMPLATEPATH . "/inc/clientes-portfolio.php") ?>
 			</div>
 		</section>
-
 <?php endwhile;
 else : endif; ?>
 <?php get_footer(); ?>
